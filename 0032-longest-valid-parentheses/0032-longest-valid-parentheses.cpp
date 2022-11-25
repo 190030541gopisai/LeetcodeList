77c57317b3ed24947914a9309fc2fbc304057ebb
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-11-25 10:56:16 +0530

    Time: 967 ms (5.02%), Space: 6.8 MB (88.54%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,58 +1,47 @@
-class Solution {
-    bool isValid(string s){
-        int n = s.length();
-        if(n == 0 || n % 2 != 0){
-            return false;
-        }
-        stack<char> stk;
-        for(auto typeOfBracket : s){
-            if(typeOfBracket == '('){
-                stk.push('(');
-            }
-            else if(!stk.empty() && stk.top() == '('){
-                stk.pop();
-            }
-            else{
-                return false;
-            }
-        }
-        return stk.empty();
-    }
-public:
-    int longestValidParentheses(string s) {
-        int n = s.length();
-        
-        if(n == 0){
-            return 0;
-        }
-        
-        int ans = 0, countOpen = 0, countClose = 0;
-        
-        for(int i = 0; i < n; i++){
-            countOpen = 0;
-            countClose = 0;
-            for(int j = i; j < n; j++){
-                if(s[j] == '('){
-                    countOpen++;
-                }else{
-                    countClose++;
+//  TC = O(N^2)
+//  SC = O(1)
+class Solution
+{
+    public:
+        int longestValidParentheses(string s)
+        {
+            int n = s.length();
+            int ans = 0, countOpen = 0, countClose = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                countOpen = 0;
+                countClose = 0;
+
+                for (int j = i; j < n; j++)
+                {
+                    if (s[j] == '(')
+                    {
+                        countOpen++;
+                    }
+                    else
+                    {
+                        countClose++;
+                    }
+
+                    int diff = countOpen - countClose;
+                    /*
+                    diff < 0 
+                    means that there are too many right parentheses. At this time, no matter what is behind it, it                       must be an illegal string, so the loop can be ended early
+                    */
+                    if (diff < 0)
+                    {
+                        break;
+                    }
+
+                   	// diff == 0 means we have both equal open and equal close
+                    if (diff == 0)
+                    {
+                        ans = max(ans, j - i + 1);
+                    }
                 }
-                
-                int diff = countOpen - countClose;
-                /*
-                diff < 0 
-                means that there are too many right parentheses. At this time, no matter what is behind it, it must be an illegal string, so the loop can be ended early
-                */
-                if(diff < 0){
-                    break;
-                }
-                
-                if(diff == 0){
-                    ans = max(ans, j - i + 1);
-                }
-            }    
+            }
+
+            return ans;
         }
-        
-        return  ans;
-    }
 };
