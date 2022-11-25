dcbe76d28d52d0d753f678c95673027f3102d8aa
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-11-25 11:47:42 +0530

    Time: 5 ms (78.05%), Space: 7.4 MB (42.94%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,47 +1,93 @@
-//  TC = O(N^2)
-//  SC = O(1)
+// TC = O(N)
+// SC = O(N)
 class Solution
 {
     public:
         int longestValidParentheses(string s)
         {
             int n = s.length();
-            int ans = 0, countOpen = 0, countClose = 0;
-
-            for (int i = 0; i < n; i++)
+            vector<int> dp(n, 0);
+            int ans = 0;
+            for (int i = 1; i < n; i++)
             {
-                countOpen = 0;
-                countClose = 0;
-
-                for (int j = i; j < n; j++)
+                if (s[i] == ')')
                 {
-                    if (s[j] == '(')
+                    int prevIndex = i - 1;
+                    int leftMostIndex = i - dp[prevIndex] - 1;
+
+                    if (prevIndex >= 0 && s[prevIndex] == '(')
                     {
-                        countOpen++;
+                        dp[i] = 2;
+                        if (i >= 2)
+                        {
+                            dp[i] += dp[i - 2];
+                        }
                     }
-                    else
+                    else if (leftMostIndex >= 0 && s[leftMostIndex] == '(')
                     {
-                        countClose++;
+                        dp[i] = 2;
+                        if (prevIndex >= 0)
+                        {
+                            dp[i] += dp[prevIndex];
+                        }
+                        if (leftMostIndex >= 1)
+                        {
+                            dp[i] += dp[leftMostIndex - 1];
+                        }
                     }
 
-                    int diff = countOpen - countClose;
-                    /*
-                    diff < 0 
-                    means that there are too many right parentheses. At this time, no matter what is behind it, it                       must be an illegal string, so the loop can be ended early
-                    */
-                    if (diff < 0)
-                    {
-                        break;
-                    }
-
-                   	// diff == 0 means we have both equal open and equal close
-                    if (diff == 0)
-                    {
-                        ans = max(ans, j - i + 1);
-                    }
+                    ans = max(ans, dp[i]);
                 }
             }
 
             return ans;
         }
 };
+
+//	//  TC = O(N^2)
+//	//  SC = O(1)
+// class Solution
+// {
+//     public:
+//         int longestValidParentheses(string s)
+//         {
+//             int n = s.length();
+//             int ans = 0, countOpen = 0, countClose = 0;
+
+//             for (int i = 0; i < n; i++)
+//             {
+//                 countOpen = 0;
+//                 countClose = 0;
+
+//                 for (int j = i; j < n; j++)
+//                 {
+//                     if (s[j] == '(')
+//                     {
+//                         countOpen++;
+//                     }
+//                     else
+//                     {
+//                         countClose++;
+//                     }
+
+//                     int diff = countOpen - countClose;
+//                     /*
+//                     diff < 0 
+//                     means that there are too many right parentheses. At this time, no matter what is behind it, it                       must be an illegal string, so the loop can be ended early
+//                     */
+//                     if (diff < 0)
+//                     {
+//                         break;
+//                     }
+
+//                    	// diff == 0 means we have both equal open and equal close
+//                     if (diff == 0)
+//                     {
+//                         ans = max(ans, j - i + 1);
+//                     }
+//                 }
+//             }
+
+//             return ans;
+//         }
+// };
