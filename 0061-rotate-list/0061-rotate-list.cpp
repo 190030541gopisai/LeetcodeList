0501f266f4b8c61f090313eae88ec66a9b6713a3
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-11-28 10:35:06 +0530

    Time: 11 ms (76.42%), Space: 11.8 MB (55.46%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,58 +1,49 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-    int getLength(ListNode* head){
-        ListNode* cur = head;
-        int countNodes = 0;
-        while(cur != nullptr){
-            countNodes++;
-            cur = cur->next;
+class Solution
+{
+    public:
+        ListNode* rotateRight(ListNode *head, int k)
+        {
+            if (head == nullptr)
+            {
+                return nullptr;
+            }
+
+            int len = 1;
+            ListNode *tail = head;
+
+            while (tail->next != nullptr)
+            {
+                tail = tail->next;
+                len++;
+            }
+
+            tail->next = head;
+            k = k % len;
+
+            if (k == 0)
+            {
+                tail->next = nullptr;
+                return head;
+            }
+            
+            for(int i = 0; i < (len - k); i++){
+                tail = tail->next;
+            }
+            
+            ListNode *newhead = tail->next;
+            tail->next = nullptr;
+            
+            return newhead;
+            
         }
-        return countNodes;
-    }
-public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(head == nullptr){
-            return nullptr;
-        }
-        
-        int N = getLength(head);
-        k = k % N;
-        
-        if(k == 0){
-            return head;
-        }
-        
-        int m = N - k;
-        ListNode* prev = nullptr, *cur = head, *newhead = nullptr;
-        
-        for(int i = 0; i < m; i++){
-            prev = cur;
-            cur = cur->next;
-        }
-        newhead = cur;
-        
-        if(prev){
-            prev->next = nullptr;
-        }
-        
-        while(cur != nullptr){
-            prev = cur;
-            cur = cur->next;
-        }
-        
-        if(prev){
-            prev->next = head;
-        }
-        
-        return newhead;
-    }
 };
