793083d522c030a7ded78018b896456f39837433
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-11-29 22:25:48 +0530

    Time: 19 ms (58.73%), Space: 7.6 MB (67.68%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+        int two = 0, three = 0, five = 0;
+        for(int i = 1; i < n; i++){
+            dp[i] = min({dp[two] * 2, dp[three] * 3, dp[five] * 5});
+            if(dp[i] == dp[two] * 2){
+                two++;
+            }
+            if(dp[i] == dp[three] * 3){
+                three++;
+            }
+            if(dp[i] == dp[five] * 5){
+                five++;
+            }
+        }
+        return dp[n-1];
+    }
+};
+
