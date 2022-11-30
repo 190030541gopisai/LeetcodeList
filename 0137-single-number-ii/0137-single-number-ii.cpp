df80b84c83efa836fa27f42f945b1c8b52dd0e1e
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-11-30 22:02:49 +0530

    Time: 23 ms (31.69%), Space: 9.5 MB (62.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,6 +2,26 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int N = nums.size();
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            int countOfOne = 0;
+            for(int j = 0; j < N; j++){
+                countOfOne += (nums[j] & (1 << i))? 1: 0;
+            }
+            ans = ((countOfOne % 3) << i) | ans;
+        }
+        return ans;
+    }
+};
+
+
+/*
+// TC = O(NlogN) + O(N)
+// SC = O(1)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int N = nums.size();
         sort(nums.begin(), nums.end());
         int repeat = 1;
         for(int i = 1; i < N; i++){
@@ -16,3 +36,5 @@ public:
         return nums[N-1];
     }
 };
+
+*/
