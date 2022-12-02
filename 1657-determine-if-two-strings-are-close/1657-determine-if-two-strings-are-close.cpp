62894023a5c21beaaf4dae46c6c5866077f9f78d
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-12-02 09:27:08 +0530

    Time: 118 ms (86.83%), Space: 20.5 MB (82.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -8,6 +8,47 @@ public:
             return false;
         }
         
+        int count1[26] = {0}, count2[26] = {0};
+        
+        for(int i = 0; i < N; i++){
+            count1[word1[i] - 'a']++;
+            count2[word2[i] - 'a']++;
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if(count1[i] != 0 && count2[i] == 0){
+                return false;
+            }
+            else if(count1[i] == 0 && count2[i] != 0){
+                return false;
+            }
+        }
+        
+        sort(count1, count1 + 26);
+        sort(count2, count2 + 26);
+        
+        for(int i = 0; i < 26; i++){
+            if(count1[i] != count2[i]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+/*
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        int N = word1.size();
+        int M = word2.size();
+        
+        if(N != M){
+            return false;
+        }
+        
         unordered_map<char, int> count1, count2;
         
         for(int i = 0; i < N; i++){
@@ -45,20 +86,22 @@ public:
     }
 };
 
-/*
- word1 = "cabbba", word2 = "abbccc"
- 
- {
-  c : 1
-  a : 2
-  b : 3
- }
 
- {
-    a: 1
-    b: 2
-    c: 3
- }
+//  word1 = "cabbba", word2 = "abbccc"
  
- check the values of two maps are equal or not
+//  {
+//   c : 1
+//   a : 2
+//   b : 3
+//  }
+
+//  {
+//     a: 1
+//     b: 2
+//     c: 3
+//  }
+ 
+//  check the values of two maps are equal or not
+
+
 */
