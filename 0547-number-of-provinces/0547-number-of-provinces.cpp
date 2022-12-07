942b08c7ef5d3b8035d035b68c757d6819d253d4
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-12-07 21:33:37 +0530

    Time: 67 ms (18.75%), Space: 13.6 MB (98.68%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,3 +1,64 @@
+class UnionFind{
+    vector<int> parent, rank;
+public:
+    UnionFind(int N){
+        parent.resize(N+1);
+        rank.resize(N+1);
+        
+        for(int i = 0; i <= N; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+        
+    }
+    int findSet(int node){
+        if(parent[node] == node){
+            return node;
+        }
+        return parent[node] = findSet(parent[node]);
+    }   
+    void unionSet(int u, int v){
+        int s1 = findSet(u);
+        int s2 = findSet(v);
+        if(rank[s1] < rank[s2]){
+            parent[s1] = s2;
+        }else if(rank[s1] > rank[s2]){
+            parent[s2] = s1;
+        }else{
+            parent[s1] = s2;
+            rank[s1]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int N = isConnected.size();
+        int count_provinces = N;
+        UnionFind uf(N);
+        
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(isConnected[i][j]){
+                    int s1 = uf.findSet(i);
+                    int s2 = uf.findSet(j);
+                    if(s1!=s2){
+                        uf.unionSet(i, j);
+                        count_provinces--;
+                    } 
+                }
+            }
+        }
+        
+        return count_provinces;
+    }
+};
+
+/*
+
+//  Tc see in youtube once i did not understand
+
 class Solution {
     
     void dfs(int i, int N, vector<vector<int>>& isConnected, vector<bool> &visited){
@@ -26,3 +87,5 @@ public:
         return count_provinces;
     }
 };
+
+*/
