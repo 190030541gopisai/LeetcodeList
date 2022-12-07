ed25557cdbfe97e5d8ebc566bacdd246935da458
190030541gopisai <75208548+190030541gopisai@users.noreply.github.com>
2022-12-07 19:22:28 +0530

    Time: 212 ms (68.43%), Space: 64.7 MB (18.63%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -16,15 +16,13 @@ public:
             return 0;
         }
         int rootVal = root->val;
-        if(rootVal < low && rootVal < high){
+        if(rootVal < low){
             return rangeSumBST(root->right, low, high);
         }
-        else if(rootVal > low && rootVal > high){
+        else if(rootVal > high){
             return rangeSumBST(root->left, low, high);
         }
-        else{
-            return rootVal + rangeSumBST(root->left, low, rootVal) +
-                    rangeSumBST(root->right, rootVal, high);
-        }
+        return rootVal + rangeSumBST(root->left, low, rootVal) +
+                         rangeSumBST(root->right, rootVal, high);
     }
 };
